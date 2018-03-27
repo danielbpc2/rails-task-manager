621d0eb1e7a7591b7feabd126947ecaca5617233
Daniel Carvalho <danielbpc2@hotmail.com>
2018-03-27 12:37:59 -0300

    added model, controller and one route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read all
+  get "tasks", to: "tasks#index", as: "index"
+  # read one
+  # CREATE
+  # update
+  # destroy
 end
