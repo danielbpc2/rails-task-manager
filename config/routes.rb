6e3d8d7a5009e13ca8719853eb229476b28e2a2f
Daniel Carvalho <danielbpc2@hotmail.com>
2018-03-27 13:42:44 -0300

    new/create functional

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # read all
   get 'tasks', to: 'tasks#index', as: 'index'
   # read one
-  get 'task/:id', to: 'tasks#show', as: 'show'
+  get 'tasks/:id', to: 'tasks#show', as: 'show'
   # CREATE
+  get 'new', to: 'tasks#new', as: 'tasks_new'
+
+  post 'tasks', to: 'tasks#create'
   # update
+
   # destroy
 end
