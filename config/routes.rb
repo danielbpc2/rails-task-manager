f8f31104049a4176a28d2915938f35d3615b78fc
Daniel Carvalho <danielbpc2@hotmail.com>
2018-03-27 13:02:29 -0300

    show view,route,action functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # read all
-  get "tasks", to: "tasks#index", as: "index"
+  get 'tasks', to: 'tasks#index', as: 'index'
   # read one
+  get 'task/:id', to: 'tasks#show', as: 'show'
   # CREATE
   # update
   # destroy
